77ecfc3c6706fe7f79c234830859bf93cc43b5be
aushamim <au.shamim0@gmail.com>
2021-09-26 23:50:39 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project Name: Goriber IT Corporation
+
+# Live Site Link: https://goriber-it-corporation.netlify.app/
+
+- Features
+- - See Awesome Developers Profile
+- - Hire Desire Developers
+- - Can See the Name and Total Cost of Hired Developers
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header-sec">
+      <h1>Hire a Developer</h1>
+      <h3>Make it work, Make it right, Make it fast.</h3>
+      <h2>Total Cost: $500000</h2>
+    </div>
+  );
+};
+
+export default Header;
